4838f13ab64cd24338d6b76f1830603ef29dd2ce
Geydarova Zulfiya <152681616+Zulya1012@users.noreply.github.com>
2024-01-04 20:04:15 +0400

    Update oddnumber.java

--- oddnumber.java
+++ oddnumber.java
@@ -1,7 +1,7 @@
 public class oddnumber {
   public static void main(String[] arg) {
     for (int i = 1; i <= 100; i++) {
-      if (i % 2 == 4) {
+      if (i % 2 == 0) {
         continue;
       }
       System.out.println(i);
